eb3c7a6da1bf795d5e0a177055c3280f794a82ca
Simon Chao <ysc406@nyu.edu>
2021-07-09 11:05:04 -0400

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by YunShou Chao on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
